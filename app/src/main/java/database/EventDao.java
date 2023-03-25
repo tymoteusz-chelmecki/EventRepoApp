d0e81b738d24e59e110ca1d2afb5dbd0412fafd4
tymoteusz <tymoteusz.chelmecki@student.uj.edu.pl>
2023-03-25 18:51:16 +0100

    Add new queries to database model classes

--- app/src/main/java/database/EventDao.java
+++ app/src/main/java/database/EventDao.java
@@ -19,6 +19,19 @@ public interface EventDao {
     @Query("SELECT * FROM events")
     LiveData<List<Event>> getAll();
 
+    @Query("SELECT * FROM events WHERE name LIKE '%' || :name || '%'")
+    LiveData<List<Event>> getByName(String name);
+
+    @Query("SELECT * FROM events WHERE day = :day AND month = :month AND year = :year " +
+            "ORDER BY year, month, day ASC")
+    LiveData<List<Event>> getForDate(int day, int month, int year);
+
+    @Query("SELECT * FROM events WHERE (day >= :fromDay AND month >= :fromMonth AND year >= :fromYear "+
+            "AND day <= :toDay AND month <= :toMonth AND year <= :toYear ) " +
+            "ORDER BY year, month, day ASC")
+    LiveData<List<Event>> getBetweenDates(int fromDay, int fromMonth, int fromYear,
+                                          int toDay, int toMonth, int toYear);
+
     @Query("DELETE FROM events")
     void deleteAll();
 }
--- app/src/main/java/database/Repository.java
+++ app/src/main/java/database/Repository.java
@@ -10,6 +10,7 @@ import com.google.common.collect.Iterables;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.function.Function;
 
 public class Repository {
     private final EventDao eventDao;
@@ -25,32 +26,61 @@ public class Repository {
     }
 
     public void insert(Event event) {
-        new EventQueryTask(eventDao, EventDao::insert).execute(event);
+        new EventStatementTask(eventDao, EventDao::insert).execute(event);
     }
 
     public void delete(Event event) {
-        new EventQueryTask(eventDao, EventDao::delete).execute(event);
+        new EventStatementTask(eventDao, EventDao::delete).execute(event);
     }
 
     public void deleteAll() {
         new DeleteAllTask(eventDao).execute();
     }
 
-    private static class EventQueryTask extends AsyncTask<Event, Void, Void> {
-        private final EventDao eventDao;
-        private final BiConsumer<EventDao, Event> queryAction;
+    public LiveData<List<Event>> getByName(String name) {
+        return eventDao.getByName(name);
+    }
 
-        public EventQueryTask(EventDao eventDao, BiConsumer<EventDao, Event> queryAction) {
+    public LiveData<List<Event>> getForDate(int day, int month, int year) {
+        return eventDao.getForDate(day, month, year);
+    }
+
+    public LiveData<List<Event>> getBetweenDates(int fromDay, int fromMonth, int fromYear,
+                                                 int toDay, int toMonth, int toYear) {
+        return eventDao.getBetweenDates(fromDay, fromMonth, fromYear, toDay, toMonth, toYear);
+    }
+
+    private static class EventStatementTask extends AsyncTask<Event, Void, Void> {
+        private final EventDao eventDao;
+        private final BiConsumer<EventDao, Event> eventStatement;
+
+        public EventStatementTask(EventDao eventDao, BiConsumer<EventDao, Event> eventStatement) {
+            super();
+            this.eventDao = eventDao;
+            this.eventStatement = eventStatement;
+        }
+
+        @Override
+        protected Void doInBackground(Event... events) {
+            Event event = Iterables.getOnlyElement(Arrays.asList(events));
+            eventStatement.accept(eventDao, event);
+            return null;
+        }
+    }
+
+    private static class EventQueryTask extends AsyncTask<Event, Void, LiveData<List<Event>>> {
+        private final EventDao eventDao;
+        private final Function<EventDao, LiveData<List<Event>>> queryAction;
+
+        public EventQueryTask(EventDao eventDao, Function<EventDao, LiveData<List<Event>>> queryAction) {
             super();
             this.eventDao = eventDao;
             this.queryAction = queryAction;
         }
 
         @Override
-        protected Void doInBackground(Event... events) {
-            Event event = Iterables.getOnlyElement(Arrays.asList(events));
-            queryAction.accept(eventDao, event);
-            return null;
+        protected LiveData<List<Event>> doInBackground(Event... events) {
+            return queryAction.apply(eventDao);
         }
     }
 
